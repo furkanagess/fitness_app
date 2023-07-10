689d9bebc8e027b305fdd51cc2c41593ead1211e
furkanagess <caglarrfurkann@gmail.com>
2023-07-10 21:31:34 +0300

    app constants updated

--- lib/product/constants/app_strings.dart
+++ lib/product/constants/app_strings.dart
@@ -7,4 +7,11 @@ class AppStrings {
   static const String fitDev = 'FitDev';
   static const String ultimateFitness = 'Your ultimate fitness companion';
   static const String start = 'Start';
+  static const String fitDevApp = 'FitDev App';
+  static const String login = 'Log in';
+  static const String enterMail = 'Enter your mail';
+  static const String enterPassword = 'Enter your password';
+  static const String dontHaveAccount = 'Dont have an account?';
+  static const String signUp = 'Sign up';
+  static const String or = 'or';
 }
--- lib/product/theme/app_theme.dart
+++ lib/product/theme/app_theme.dart
@@ -15,7 +15,7 @@ class AppTheme {
   ColorScheme get _colorScheme => const ColorScheme(
         brightness: Brightness.light,
         primary: Color(0xFFF12824), // used
-        onPrimary: Color(0xFFDFDEDE),
+        onPrimary: Color(0xFFDFDEDE), // used
         secondary: Colors.white, // used
         onSecondary: Colors.white,
         error: Colors.red,
