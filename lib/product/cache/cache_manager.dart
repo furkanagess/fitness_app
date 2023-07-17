8eb7f91ed12cc81c860d651fb6a6c6b0c8e1aa05
furkanagess <caglarrfurkann@gmail.com>
2023-07-17 23:40:27 +0300

    cache manager demo added

---
+++ lib/product/cache/cache_manager.dart
@@ -0,0 +1,15 @@
+// import 'package:shared_preferences/shared_preferences.dart';
+
+// class CacheManager {
+//   static CacheManager _instance = CacheManager._init();
+//   static CacheManager get instance {
+//       return _instance;
+    
+//   }
+// CacheManager._init()
+
+
+// SharedPreferences _preferences;
+
+
+// }
