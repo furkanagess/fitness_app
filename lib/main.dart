ef467373ccd310a2e1d77892140525fe90593ab7
furkanagess <caglarrfurkann@gmail.com>
2023-07-26 23:33:22 +0300

    general fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:fitness_app/feature/main_page.dart';
+import 'package:fitness_app/feature/view/login/login_view.dart';
 import 'package:fitness_app/feature/view/onboard/onboard_view.dart';
+import 'package:fitness_app/feature/view/playlist/playlist_view.dart';
 import 'package:fitness_app/product/constants/app_strings.dart';
+import 'package:fitness_app/product/constants/navigation_constants.dart';
 import 'package:fitness_app/product/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -15,6 +19,11 @@ class MyApp extends StatelessWidget {
       title: AppStrings.appName,
       theme: AppTheme().theme,
       home: const OnboardView(),
+      routes: {
+        NavigationConstants.playlist: (context) => const PlaylistView(),
+        NavigationConstants.login: (context) => const LoginView(),
+        NavigationConstants.main: (context) => const MainPage(),
+      },
     );
   }
 }
